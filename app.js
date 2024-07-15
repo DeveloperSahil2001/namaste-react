35651d8da3ccebb882c212905fab373f2fe5c62a
Sahil <sahil@Netomis-MacBook-Pro.local>
2024-07-15 19:38:24 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+{/*
+    <div id="parent">
+        <div id="child">
+            <h1>Hi H1</h1>
+            <h2>Hi H2</h1>
+        </div>
+        <div id="child2">
+            <h1>Hi H1</h1>
+            <h2>Hi H2</h1>
+        </div>
+    </div>
+*/}
+
+
+// const header = React.createElement("h1", {id: "heading"}, "Hello world from react!")
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+    [React.createElement(
+        "div", { id: "child" },
+        [React.createElement("h1", {}, "Hi H1"), React.createElement("h2", {}, "Hi H2")]
+    ),
+    React.createElement(
+        "div", { id: "child2" },
+        [React.createElement("h1", {}, "Hi H1"), React.createElement("h2", {}, "Hi H2")]
+    )]
+)
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(parent)
